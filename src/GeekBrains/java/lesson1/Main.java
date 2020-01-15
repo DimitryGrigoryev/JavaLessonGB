94d148f757bb24c1e9d31b3a5ed2757dab6b5e61
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-15 23:45:45 +0700

    add wrong

--- src/GeekBrains/java/lesson1/Main.java
+++ src/GeekBrains/java/lesson1/Main.java
@@ -3,7 +3,7 @@ package GeekBrains.java.lesson1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!")
         System.out.println("Line");
     }
 }
