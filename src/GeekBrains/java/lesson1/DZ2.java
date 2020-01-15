c694c7402756addc94b9f1c2022531639cfd61b3
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-16 00:37:43 +0700

    add new dz 2

---
+++ src/GeekBrains/java/lesson1/DZ2.java
@@ -0,0 +1,19 @@
+package GeekBrains.java.lesson1;
+
+public class DZ2 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float, double, char, boolean;
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+        long d = 54775807;
+        float e = 12.23f;
+        double f = 123.123;
+        char chr = 'A';
+        String chr1 = "Символы";
+        boolean answer = true;
+
+
+
+    }
+}
