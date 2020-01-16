565a16ee1afa20c793b42c868ac4cb4eac06653b
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-16 20:37:34 +0700

    add all dz

---
+++ src/GeekBrains/java/lesson1/DZ6.java
@@ -0,0 +1,12 @@
+package GeekBrains.java.lesson1;
+
+public class DZ6 {
+    public static boolean isNegative (int v) {
+        return v < 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isNegative(0));
+    }
+
+}
