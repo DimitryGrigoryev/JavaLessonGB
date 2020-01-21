169e9a8e6dfb30b4cbae4c3cdebac65ead8c1498
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-21 23:49:59 +0700

    add1

--- src/lesson2/HomeWork2/HW2_DZ5.java
+++ src/lesson2/HomeWork2/HW2_DZ5.java
@@ -4,6 +4,10 @@ public class HW2_DZ5 {
     public static void main(String[] args) {
         System.out.println(5);
         System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
 
     }
 }
