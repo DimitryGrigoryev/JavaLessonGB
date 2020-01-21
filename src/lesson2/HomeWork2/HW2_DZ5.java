df1ad4473627135d92a2ff063d71d1bee4eb1aa4
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-21 23:42:41 +0700

    Homework 2.1

--- src/lesson2/HomeWork2/HW2_DZ5.java
+++ src/lesson2/HomeWork2/HW2_DZ5.java
@@ -4,8 +4,6 @@ public class HW2_DZ5 {
     public static void main(String[] args) {
         System.out.println(5);
         System.out.println(5);
-        System.out.println(5);
-        System.out.println(5);
 
     }
 }
