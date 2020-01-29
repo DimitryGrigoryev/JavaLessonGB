e2eb28349c250ebf23b2178a5af1599dcec2da55
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-29 19:40:48 +0700

    dz 3 override

--- src/lesson3/DZ3/Puzzle.java
+++ src/lesson3/DZ3/Puzzle.java
@@ -1,15 +1,20 @@
 package lesson3.DZ3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 
 public class Puzzle {
+
+    public static final String[] WORDS = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
     public static void main(String[] args) {
         String puzzles = getRandomWord().toLowerCase();
-        System.out.println("Попробуйте угадать загаданное слово.");
+        System.out.println("Попробуйте угадать загаданное слово.\n" + Arrays.toString(WORDS));
         String temp = "###############";
+        System.out.println("Введите свой ответ: ");
         Scanner scanner = new Scanner( System.in );
         String answer = toLowerCase(scanner.nextLine());
         while (!answer.equals(puzzles)) {
@@ -21,7 +26,7 @@ public class Puzzle {
         System.out.printf("Вы угадали! загаданное слово %s !", puzzles);
     }
     private static Object getOpenLetters(String puzWord, String answerStr, String temp) {
-        int[] len = {puzWord.length(),answerStr.length(),temp.length()};
+        int[] len = {puzWord.length(), answerStr.length(), temp.length()};
         int min = getMinLength(len);
         char[] arr = temp.toCharArray();
         for (int i = 0; i < min; i++) {
@@ -43,9 +48,8 @@ public class Puzzle {
             return min;
     }
     private static String getRandomWord() {
-        String[] words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
         Random random =new Random();
-        int i = random.nextInt(words.length -1);
-        return words[i];
+        int i = random.nextInt(WORDS.length);
+        return WORDS[i];
     }
 }
