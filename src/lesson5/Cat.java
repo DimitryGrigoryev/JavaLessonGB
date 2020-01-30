09ef14b6a5cbbc299e72a269c3fe103f377f8695
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-01-30 20:00:57 +0700

    lesson 5

--- src/lesson5/Cat.java
+++ src/lesson5/Cat.java
@@ -1,9 +1,61 @@
 package lesson5;
 
-public class Cat {
-    String name;
-    String color;
-    int age;
+public class  Cat {
+
+    private static int legsNumber = 4;
+
+    private String name;
+    private String color;
+    private int age;
+
+    public static void  printLegs() {
+
+        System.out.println(legsNumber);
+    }
+
+    public Cat() {
+    }
+
+//    public Cat() {
+//        name = "Barsik";
+//        color = "Red";
+//        age = 3;
+//    }
+
+    public Cat(String name, String color, int age) {
+        this.name = name.toLowerCase();
+        this.color = color;
+        this.age = age;
+    }
+
+    public Cat(String name, String color) {
+        this(name, color, 0);
+    }
+
+    void sayMeow() {
+        System.out.println("Meow!");
+    }
+
+    public Cat(String name) {
+        this(name, null,0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
     @Override
     public String toString() {
--- src/lesson5/TestCat.java
+++ src/lesson5/TestCat.java
@@ -2,16 +2,31 @@ package lesson5;
 
 public class TestCat {
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        cat1.name = "Barsik";
-        cat1.color = "Red";
-        cat1.age = 3;
-        System.out.println(cat1);
+        Cat cat1 = new Cat("Barsik", "red", 3);
+//        cat1.name = "Barsik";
+//        cat1.color = "Red";
+//        cat1.age = 3;
+        System.out.println("Cat1 :" + cat1);
 
-        Cat cat2 = new Cat();
-        cat2.name = "Murzik";
-        cat2.color = "Gray";
-//        cat2.age = 4;
-        System.out.println(cat2);
-    }
+        Cat cat2 = new Cat("Barsik", "red");
+        System.out.println("Cat2 : " + cat2);
+
+        Cat cat3 = new Cat("Murzik");
+        System.out.println("Cat3 : " + cat3);
+        System.out.println("_________");
+
+        cat3 = cat2;
+        System.out.println("Cat3 : " + cat3);
+        cat3.setName("Муська");
+        System.out.println("Cat2 : " + cat2);
+        System.out.println("Cat3 : " + cat3);
+        cat3.sayMeow();
+
+        Cat.printLegs();
+
+
+
+
+
+      }
 }
