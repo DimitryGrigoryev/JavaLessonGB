6933dfba41f5c2eb057d0a2af988689ecd07daef
Dimitrii Grigoryev <grigoryevdk@gmail.com>
2020-02-16 00:34:31 +0700

    lesson 7/1

--- src/lesson7/oop/Test7.java
+++ src/lesson7/oop/Test7.java
@@ -4,7 +4,7 @@ public class Test7 {
 
     public static void main(String[] args) {
         Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(100);
+        Plate plate = new Plate(4);
         plate.info();
         cat.eat(plate);
         plate.info();
